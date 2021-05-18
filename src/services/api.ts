d02c3b1baff39189d5b038a9c6f20fbdbfe8f7c5
antonylrds <antonylrds@gmail.com>
2021-05-18 19:43:38 -0300

    Teste na variavel de ambiente LIVE

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+console.log(process.env.LIVE);
+
 const api = axios.create({
   baseURL: process.env.LIVE ? 'https://abare-project.herokuapp.com/' : 'http://localhost:3333/',
 });
